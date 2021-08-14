331d5ba7d286eed5b9e25a997f1ebf0f7967aa4f
msb76 <81312619+msb76@users.noreply.github.com>
2021-08-14 12:33:08 +0300

    Capstone Project

---
+++ Notebooks/data_des_.py
@@ -0,0 +1,118 @@
+data_des = {
+    "gender": "Gender",
+    "age": "Age",
+    "education":"Education Level",
+    "major": "Major",
+    "regions": "administrative regions",
+    
+    "eservice_usage": "How often do you use the Internet and Internet-related services? (E.g. Email, WhatsApp, News, YouTube",
+    
+    "device_skill_level" : "What are your Internet/ Digital devices skills level",
+    "freq_used" : "Of the following, which digital devices do you use regularly? Tick all that apply",
+    
+    "connection_type" : "What type of connectivity services do you use in your daily activities? Tick all that apply",
+    
+    "used_purpose" : "For what purposes do you use the Internet? Tick all that apply",
+    "PC_OS":"What operating systems do you use on your desktop/laptop? Tick all that apply",
+    
+    'phone_OS':"What operating systems do you use on your Smartphone/tablet? Tick all that apply",
+    
+    'security_apps':"Some of the most commonly used security tools and applications for laptops, tablets, mobiles, etc. are given below. Select which of these you have used on your digital devices. Tick all that apply",
+    
+    'secure_level':"How secure do you feel your digital devices (e.g. computers and phones) are?",
+    'check_legitimacy':"Some security practices are described below. Please choose your common reaction for each practice: [I check the legitimacy of a website before accessing it .]", 
+    
+    "pass_my_info":"Some security practices are described below. Please choose your common reaction for each practice: [I create a password that contains my personal information (e.g. last name, date of birth)]",
+    
+    "clicking_on_banners":"Some security practices are described below. Please choose your common reaction for each practice: [I am aware of the danger when clicking on banners, advertisements or pop-up screens that appear when surfing the Internet]",
+    
+    "privacy_settings":"Some security practices are described below. Please choose your common reaction for each practice : [I give due attention to privacy settings on my social media account(s) (e.g. Facebook)]",
+    
+    "SM_services_protect_info":"Some security practices are described below. Please choose your common reaction for each practice: [Social media services protect my personal information]",
+    
+    "terms_conditions":"Some security practices are described below. Please choose your common reaction for each practice: I read the terms and conditions carefully before using any website",
+    
+    "change_pass_frequently":"Some security practices are described below. Please choose your common reaction for each practice: [I change the passwords of important accounts (such as online banking) frequently ]",
+    
+    "feel_safe_public_Wi-Fi":"Some security practices are described below. Please choose your common reaction for each practice: [I feel safe when using public Wi-Fi]",
+    
+    "no_value_to_hackers?":"Some security practices are described below. Please choose your common reaction for each practice: [I feel my digital devices (computer, smartphones) has no value to hackers, they do not target me]",
+    
+    "install_software_updates":"Some security practices are described below. Please choose your common reaction for each practice: [I regularly install software updates ]",
+    
+    "careful_clicking_on_links":"Some security practices are described below. Please choose your common reaction for each practice: [I am careful about clicking on links in an email or social media post]",
+    
+    "Internet_security":"What digital devices do you have Internet security on (e.g. anti-virus)?",
+    
+    "Internet_security_up_to_date":"If you use Internet security (e.g. anti-virus), is this kept up to date in terms of threat filters and signatures?",
+    
+    "updated_about_CC_(offline Sources)":"How do you keep yourself updated about cyber crime? Tick all that apply (offline Sources)",
+    
+    "updated_about_CC(_Online Sources)":"How do you keep yourself updated about cyber crime? Tick all that apply (Online Sources)",
+    
+    "avoid_disclosing":"What is your opinion of each of the following statements? Select the appropriate response for each: [I think one should avoid disclosing personal information online]", 
+    
+    "risk_increased":"What is your opinion of each of the following statements? Select the appropriate response for each: [I feel that the risk of becoming a victim of cyber crime has increased in the past year.]",
+    
+    "not_secure":"What is your opinion of each of the following statements? Select the appropriate response for each.: [I am concerned that my online personal information is not secure enough ]",
+    
+    "protected":"What is your opinion of each of the following statements? Select the appropriate response for each: [I feel that I am well protected against cyber crime]",
+    
+    "Accept_surveillance":"What is your opinion of each of the following statements? Select the appropriate response for each: [I am willing to accept increased Internet surveillance from the government if it can enhance Internet security ]",
+    
+    "law_manage":"What is your opinion of each of the following statements? Select the appropriate response for each: [I believe that the laws in effect are effective in managing the cyber crime problem]",
+    
+    "informed_threat":"What is your opinion of each of the following statements? Select the appropriate response for each: [I feel informed about the threat of cyber crime]",
+    
+    "phishing_emails":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Received phishing emails (e.g. asking for money, personal information ",
+    
+    "identity_theft":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Identity theft (somebody stealing your personal data and impersonating you",
+    
+    "malware":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Malware (e.g. virus) infection of a device]",
+    
+    "unable_access_online":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Being unable to access online services (e.g. banking services)",
+    
+    "encountering_extremism":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Accidentally encountering material that promotes hatred",
+    
+    "online_extortion":"There are several activities that constitute cyber crimes. How often have you experienced or been victim of the following situations? Select the appropriate response for each: [Online extortion (a demand for money to avert or stop extortion",
+    
+    "concerned_about_identity_theft":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each [I am concerned about identity theft (somebody stealing your personal data and impersonating",
+    
+    "concerned_about_encountering_child_pornography":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each: [I am not concerned about accidentally encountering child pornography online]",
+    
+    "concerned_about_receiving_phishing_emails":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each: [I am concerned about receiving phishing emails (e.g. asking for money, personal information",
+    
+    "concerned_about_not_being_able_to_access_online":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each: [I am concerned about not being able to access online services (e.g. banking services)",
+    
+    "concerned_about_encountering_religious_extremism_material_":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each: [I am concerned about accidentally encountering material that promotes hatred or religious",
+    
+    "opinion_about_online_extortion":"Some of the most common cyber crimes are presented below. What is your opinion of each of the following statements? Select the appropriate response for each: [Online extortion (a demand for money to avert or stop extortion, or to avert scandal)]",
+    
+    "Delete":"Other, please specify", 
+    "future_feeling_CC":"What do you feel about the threat of cyber crimes in the future?",
+    
+    "government_responsible":"Considering each of the following parties, please rate the extent to which you believe they are responsible for raising awareness of cyber crime: [The government]",
+    
+    "media_responsible":"Considering each of the following parties, please rate the extent to which you believe they are responsible for raising awareness of cyber crime: [The media]",
+    
+    "tele_responsible":"Considering each of the following parties, please rate the extent to which you believe they are responsible for raising awareness of cyber crime: [Those offering online/Internet-based services (e.g. banks, online retailers, telecommunication companies",
+    
+    "user_responsible":"Considering each of the following parties, please rate the extent to which you believe they are responsible for raising awareness of cyber crime: [User itself]",
+    
+    "education_responsible":"Considering each of the following parties, please rate the extent to which you believe they are responsible for raising awareness of cyber crime: [Education system]",
+    
+    "other_responsible":"Other, please specify_1",
+    "government_role":"What do you think the role of the government should be in combating cyber crimes? Please tick all that apply.",
+    
+    "victim":"Have you been a victim of cyber crime? (E.g. lost data or email account, device infected with virus or spyware, stole your picture/s or digital device/s).",
+    
+    "when_being_victim":"When you had been a victim of cyber crime, did you report it?",
+    "contact_to_victim":"To whom did you report or contact? (Please check all that apply)",
+    "not_victim":"What was/were the reason/s? (Please check all that apply)",
+    "not_victim_would_like_to_report":"If you become a victim of cyber crime would you like to report it?",
+    "not_victim_aggree_to_report":"To whom would you report or contact? (Please check all that apply)",
+    "not_victim_not_aggree_to_report":"What is/are the reason/s? (Please check all that apply)" ,
+    
+        }
+
+
---
+++ README.md
@@ -0,0 +1,65 @@
+# Cybercrime Awareness In Saudi Arabia
+
+## Project Intro/Objective
+
+The purpose of this project is predict if someone is a victim or not by using many different models.
+
+## Business Statement 
+<img src='Images/intro_image.png' width="800" style="align:middle"/>
+
+### Methods Used
+* Train Test Split
+* OneHot Encoder
+* Label Encoder
+* Select KBest
+* PCA
+* Pipeline
+* Grid SearchCV
+* Logistic Regression
+* Decision Tree Classifier
+* Random Forest Classifier
+* SVC
+* Confusion Matrix
+
+
+### Technologies
+* jupyter
+* Tableau
+* Pandas
+* Numpy 
+* Matplotlib
+* Seaborn
+* Sklearn
+
+## Project Description
+
+Using a cybercrime dataset for bulding a model. At the start , cleaning the data and rechange ,rearrange some variables. After that, start making many plot to show our insights in this dataset. After EDA , Starting to split the data to train and test. Then, using differnt techniques of feature engineering for some columns. The baseline of the target column has been created. For the modling part the project starts with LogisticRegression. After that, Decision Tree Classifier and Random Forest Classifier. The final model is SVC. Using pipeline, grid searchCV and confusion matrix with these models and some models using PCA while others are using select kbest. The final step is compare all models together and see which models is the best for out business case.
+
+
+## Contents
+
+- data cleaning
+- data exploration
+- data visualization
+- feature engineering
+- baseline
+- different modling techniques
+
+## Report
+[Report Link](https://github.com/Abdullah-albutih/Cybercrime/blob/main/Report/CyberCrime_Awareness_Report.ipynb)
+
+
+## Tableau Dasboard
+[Dashboard Link](https://public.tableau.com/app/profile/mohammed.saud/viz/project_16262227713860/Dashboard1)
+
+
+## Team Members
+
+|Name     |  Github Account   | 
+|---------|-------------------|
+| Abdullah Albutih|[Abdullah-albutih](https://github.com/Abdullah-albutih)|
+| Abdulrahman Aljubaylan|[abdulrahman0111](https://github.com/abdulrahman0111)|
+| Ahmed Altowairqi|[AhmadBasha](https://github.com/AhmadBasha)|
+| Mohammed Albesher|[msb76](https://github.com/msb76)|
+| Raghad Almeataz|[Raghad-Almeataz](https://github.com/Raghad-Almeataz)|
+
